de4be273e461c9694aa848ce6459072a46791955
Martin Kinoo <martkinoo@gmail.com>
2025-07-14 15:00:31 +0200

    feat: add weather context

---
+++ src/services/context/WeatherContext.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import { weatherDomains } from '../../stores/weatherDomainsState.svelte';
+	import { weatherState } from '../../stores/weatherState.svelte';
+
+	setContext('weather-domains', () => weatherDomains);
+	setContext('weather-state', () => weatherState);
+
+  let {children} = $props();
+</script>
+
+{@render children()}
+
+
