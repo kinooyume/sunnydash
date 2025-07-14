01e6088f0e2305a0f73025cd5da63699c1e5b023
Martin Kinoo <martkinoo@gmail.com>
2025-07-14 15:01:12 +0200

    feat: add Header with citySearch

---
+++ src/app/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import CitySearch from "../components/features/CitySearch/CitySearch.svelte";
+
+</script>
+
+<header>
+<CitySearch  placeholder="Search for a city" />
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 1.5rem;
+		background: linear-gradient(90deg, #3f51b5 0%, #5c6bc0 100%);
+		box-shadow: 0 2px 8px rgba(63,81,181,0.10);
+		border-bottom-left-radius: 18px;
+		border-bottom-right-radius: 18px;
+	}
+</style>
