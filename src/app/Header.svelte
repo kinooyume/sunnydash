d012732e56be2f9f8a0c854003701d4bf8e773d5
Martin Kinoo <martin@kinoo.dev>
2025-08-13 17:26:32 +0200

    style: Header: remove border radius

--- src/app/Header.svelte
+++ src/app/Header.svelte
@@ -15,7 +15,7 @@
 		padding: 1.5rem;
 		background: linear-gradient(90deg, #3f51b5 0%, #5c6bc0 100%);
 		box-shadow: 0 2px 8px rgba(63,81,181,0.10);
-		border-bottom-left-radius: 18px;
-		border-bottom-right-radius: 18px;
+		/* border-bottom-left-radius: 18px; */
+		/* border-bottom-right-radius: 18px; */
 	}
 </style>
